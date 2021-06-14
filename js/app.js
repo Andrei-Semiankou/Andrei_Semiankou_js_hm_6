06e5c1a6334aa7740234cbabe57580036e64f69a
Andrei-Semiankou <semenkovandrey1998@gmail.com>
2021-06-14 18:33:49 +0300

    task 10

--- js/app.js
+++ js/app.js
@@ -183,17 +183,11 @@ console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sher
 
 // Задание 10
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
-function getSortedUniqueSkills (users)  {
-  return users.reduce((total, user) => {
-     user.skills.forEach((skill) => {
-        if (total.indexOf(skill) === -1){
-        total.push(skill);
-        }
-
-     })
-     return total
-  },[]) 
-      
-};
+const getSortedUniqueSkills = users => {
+       return users
+           .reduce((acc, { skills }) => acc.concat(skills), [])
+           .sort()
+           .filter((skill, index, skills) => index === skills.indexOf(skill));
+   };
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
